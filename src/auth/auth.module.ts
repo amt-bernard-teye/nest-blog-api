4b1bb15c422f6549de9201771149772aa298179e
Bernard Teye <bernard.teye@amalitech.com>
2024-06-25 10:09:32 +0000

    add auth.service.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { DatabaseModule } from 'src/database/database.module';
+import { AuthService } from './auth.service';
 
 @Module({
   controllers: [AuthController],
-  imports: [DatabaseModule]
+  imports: [DatabaseModule],
+  providers: [AuthService]
 })
 export class AuthModule {}
