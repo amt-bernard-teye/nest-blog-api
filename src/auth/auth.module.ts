5b427293f2edfc2b4e662d9c86ef812e269f8dbf
Bernard Teye <bernard.teye@amalitech.com>
2024-06-25 10:09:00 +0000

    import database module into auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  controllers: [AuthController]
+  controllers: [AuthController],
+  imports: [DatabaseModule]
 })
 export class AuthModule {}
