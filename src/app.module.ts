3d6f7d69bf811cb0265894b4051d53214784ae60
Bernard Teye <bernard.teye@amalitech.com>
2024-06-25 10:22:41 +0000

    add mailer.module.ts and import into app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
+import { MailerModule } from './mailer/mailer.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
-    AuthModule
+    AuthModule,
+    MailerModule
   ],
 })
 export class AppModule {}
