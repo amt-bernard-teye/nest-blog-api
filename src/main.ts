11fc38c0af0ea7d96be206c4541e266ace8d8710
Bernard Teye <bernard.teye@amalitech.com>
2024-06-25 11:10:40 +0000

    refactor main.ts to enable cors and provide access to static asset files

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix("api");
+  app.enableCors();
+  app.useStaticAssets(join(__dirname, "../", "public"));
 
   const config = new DocumentBuilder()
     .setTitle("Blog Api")
