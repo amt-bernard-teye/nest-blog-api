486f71f798d3d0cf7c8cda7efbd55a60cb36d4c3
Bernard Teye <bernard.teye@amalitech.com>
2024-06-26 08:53:53 +0000

    add register function to AuthService class, use that in the AuthController during account registration request handler

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post, UseInterceptors, ValidationPipe } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { MessageOnlyInterceptor } from 'src/shared/interceptors/message-only.interceptor';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    @Post("register")
+    @UseInterceptors(MessageOnlyInterceptor)
+    async register(@Body(ValidationPipe) body: RegisterDto) {
+        if (body.password !== body.confirmPassword) {
+            throw new BadRequestException("Passwords do not match each other");
+        }
+
+        const {confirmPassword, ...userToRegister} = body;
+        await this.authService.register(userToRegister);
+
+        return "Check your email inbox to complete your registration process";
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import * as bcryptjs from "bcryptjs";
+
+import { UserRepository } from 'src/database/repository/user.repository';
+import { RegisteringUser } from './interface/registering-user.interface';
+import { AccountStatus, Role } from '@prisma/client';
+import { RegisterAccountService } from 'src/mailer/service/register-account.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private userRepo: UserRepository,
+        private registerAccService: RegisterAccountService,
+        private configService: ConfigService,
+        private jwtService: JwtService
+    ) { }
+
+    async register(userToRegister: RegisteringUser) {
+        const existingUser = await this.userRepo.find(userToRegister.email);
+
+        if (existingUser) {
+            throw new BadRequestException("Email already exist");
+        }
+
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(userToRegister.password, salt);
+
+        try {
+            const registeredUser = await this.userRepo.add({
+                role: Role.READER,
+                name: userToRegister.name,
+                email: userToRegister.email,
+                accountStatus: AccountStatus.PENDING,
+                password: hashedPassword
+            });
+    
+            const tokenExpiration = "3h";
+            const secretKey = this.configService.get("SECRET_KEY");
+            const token = this.jwtService.sign({
+                sub: registeredUser.id,
+                email: registeredUser.email
+            }, {expiresIn: tokenExpiration, secret: secretKey}); 
+    
+            await this.registerAccService.sendMail({
+                email: registeredUser.email,
+                name: registeredUser.name,
+                token
+            });
+        }
+        catch(error) {
+            console.log(error);
+            throw new InternalServerErrorException("Something went wrong");
+        }
+    }
+}
