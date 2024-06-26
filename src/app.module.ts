51d72a2f390c9f8264d6b4b1ea201b2e2b1916f8
Bernard Teye <bernard.teye@amalitech.com>
2024-06-26 08:08:26 +0000

    add shared.module.ts, import into app.module as part of imports array property and register ConfigureModule and JwtModule too

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { MailerModule } from './mailer/mailer.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    JwtModule.register({ global: true }),
     DatabaseModule,
     AuthModule,
-    MailerModule
+    MailerModule,
+    SharedModule
   ],
 })
 export class AppModule {}
