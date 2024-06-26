eb0f4105f3f528780761ff7aa34527c858fffcf3
Bernard Teye <bernard.teye@amalitech.com>
2024-06-26 08:15:53 +0000

    add message-only.interceptor.ts and import as provider into shared.module.ts

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,4 +1,9 @@
 import { Module } from "@nestjs/common";
+import { MessageOnlyInterceptor } from "./interceptors/message-only.interceptor";
 
-@Module({})
+@Module({
+    providers: [
+        MessageOnlyInterceptor
+    ]
+})
 export class SharedModule {}
