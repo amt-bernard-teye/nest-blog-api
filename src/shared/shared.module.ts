5fadec080a8de98835dadfdfb5d3f3f31cf058b7
Bernard Teye <bernard.teye@amalitech.com>
2024-06-26 09:44:59 +0000

    add response-message.decorator.ts(will be used be the data-message.interceptor.ts to retrieve the metadata when adding the message) and data-message.interceptor.ts containing DataMessageInterceptor class that implements NestInterceptor interface

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { MessageOnlyInterceptor } from "./interceptors/message-only.interceptor";
+import { DataMessageInterceptor } from "./interceptors/data-message.interceptor";
 
 @Module({
     providers: [
-        MessageOnlyInterceptor
+        MessageOnlyInterceptor,
+        DataMessageInterceptor
     ]
 })
 export class SharedModule {}
