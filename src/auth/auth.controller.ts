88c7e2041274eb66e302c1b4457f4fce36857c27
Bernard Teye <bernard.teye@amalitech.com>
2024-06-26 10:02:47 +0000

    add login route handler in AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,11 @@
 import { BadRequestException, Body, Controller, Post, UseInterceptors, ValidationPipe } from '@nestjs/common';
+
+import { MessageOnlyInterceptor } from 'src/shared/interceptors/message-only.interceptor';
+import { DataMessageInterceptor } from 'src/shared/interceptors/data-message.interceptor';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
-import { MessageOnlyInterceptor } from 'src/shared/interceptors/message-only.interceptor';
+import { LoginDto } from './dto/login.dto';
+import { ResponseMessage } from 'src/shared/decorators/response-message.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -18,5 +22,12 @@ export class AuthController {
         await this.authService.register(userToRegister);
 
         return "Check your email inbox to complete your registration process";
+    }
+
+    @Post("login")
+    @ResponseMessage("Access granted")
+    @UseInterceptors(DataMessageInterceptor)
+    async login(@Body(ValidationPipe) body: LoginDto) {
+        return await this.authService.login(body.email, body.password);
     }
 }
