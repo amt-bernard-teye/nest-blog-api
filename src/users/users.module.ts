28b55ade0cf03417719581318682e67965b0e273
Bernard Teye <bernard.teye@amalitech.com>
2024-06-27 08:32:08 +0000

    import DatabaseModule, SharedModule and MailerModule into users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { DatabaseModule } from 'src/database/database.module';
+import { MailerModule } from 'src/mailer/mailer.module';
+import { SharedModule } from 'src/shared/shared.module';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
+  imports: [DatabaseModule, MailerModule, SharedModule]
 })
 export class UsersModule {}
